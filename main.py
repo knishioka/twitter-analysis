9a15243dd92f2397ab5d5eff269105c71eda7463
Kenichiro Nishioka <kenichiro.nishioka@bebit.com>
2021-02-10 09:58:54 +0900

    Add stop words and use collocations option

--- main.py
+++ main.py
@@ -15,9 +15,33 @@ def main():
     ]
     nouns = list(itertools.chain.from_iterable(map(extract_list_of_words, tweets)))
     fpath = "~/Library/Fonts/ipaexg.ttf"
-    stop_words = ["https"]
+    stop_words = [
+        "https",
+        "朝活",
+        "今日",
+        "の",
+        "さん",
+        "t",
+        "co",
+        "おは",
+        "さん",
+        "お",
+        "ん",
+        "おはよう",
+        "ー",
+        "こと",
+        "よう",
+        "1日",
+        "朝",
+        "人",
+        "私",
+        "活",
+        "中",
+        "日",
+        "ちゃん",
+    ]
     wordcloud = WordCloud(
-        font_path=fpath, background_color="white", width=900, height=500, stopwords=stop_words
+        font_path=fpath, background_color="white", width=900, height=500, stopwords=stop_words, collocations=False
     ).generate(" ".join(nouns))
 
     plt.figure(figsize=(15, 12))
